c73a9cd9d397e5acc3c03b41928e1ebf4c6e8348
Myonline88 <63910637+myonline88@users.noreply.github.com>
2020-05-01 23:02:33 +0800

    Add files via upload

---
+++ Joystick_RX.ino
@@ -0,0 +1,137 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h> 
+ 
+/*----- Declare all constant pin to be used ----*/
+#define CE_PIN   9    //2.4GHz CE_pin of NRF24L01 module
+#define CSN_PIN 10    //2.4GHz CSN_pin of NRF24L01 module
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; // This is the transmit pipe to communicate the two module
+RF24 radio(CE_PIN, CSN_PIN);        // Activate  the Radio  
+int joystick[7];  // 7 element array holding the Joystick readings
+
+int motorspeed;
+int motor1pin1 = 3;       // left motors 2
+int motor1pin2 = 4;       // left motor 1
+int motor2pin1 = 7;       // right motor 1
+int motor2pin2 = 8;       // right motor 2
+int EnableA = 5;        // enable pin for motor 1
+int EnableB = 6;        // enable pin for motor 2
+
+Servo panServo;         // the servo for panning
+Servo tiltServo;        // the servo for tilting
+
+void setup() { 
+ Serial.begin(9600); /* Opening the Serial Communication */
+ delay(1000);
+ Serial.println("Nrf24L01 Receiver Starting");
+
+ radio.begin();
+ radio.setPALevel(RF24_PA_MIN);
+ radio.setAutoAck(true);
+ radio.openReadingPipe(1,pipe);
+ radio.startListening();
+ radio.powerUp();
+
+ pinMode(motor1pin1, OUTPUT);
+ pinMode(motor1pin2, OUTPUT);
+ pinMode(motor2pin1, OUTPUT);
+ pinMode(motor2pin2, OUTPUT);
+
+ pinMode(EnableA, OUTPUT);
+ pinMode(EnableB, OUTPUT);
+
+ panServo.attach(A2);      // pan servo on A2
+ tiltServo.attach(A3);     // tilt servo on A3
+ panServo.write(90);       // sets pan servo to middle
+ tiltServo.write(50);      // sets tilt servo to middle
+}
+
+void loop()   /****** MAIN LOOP: RUNS CONSTANTLY ******/
+{
+  if (radio.available())
+  {
+    // Read the data payload until we've received everything
+       radio.read( joystick, sizeof(joystick) );
+       nrf24l01();    // all the joystick events in this function
+   }  else  {    
+      Serial.println("No radio available");
+      halt();     // if no radio transmission is received, the car stops
+  }
+}//--(end main loop )---
+
+void nrf24l01() { 
+      radio.powerUp();
+      Serial.print("Y =");
+      Serial.print(joystick[0]);
+      Serial.print("\tX =");      
+      Serial.print(joystick[1]);
+ 
+      // motors for moving Forward or Backward
+      if(joystick[0] >65) { motorspeed=joystick[0]+134; forward(); }
+      if(joystick[0] <55) { motorspeed=255-joystick[0]; backward(); }
+      if(joystick[0] >=55 && joystick[0] <=65) { halt();  motorspeed=0;}
+      Serial.print("\tY-Speed: ");      
+      Serial.print(motorspeed);
+    
+      // motors for moving Left or Right
+      if(joystick[1] >60) { motorspeed = 255; turnleft(); }
+      if(joystick[1] <=55) { motorspeed = 255; turnright(); }
+      if(joystick[1] >=60 && joystick[1] <=65) { halt(); motorspeed=0; }
+      Serial.print("\tX-Speed: ");      
+      Serial.print(motorspeed);
+
+      // place your additional joystick commands received from the TX
+      //
+ }
+
+void backward() {
+   analogWrite(EnableA, motorspeed);
+   analogWrite(EnableB, motorspeed); 
+   digitalWrite(motor1pin1, HIGH);
+   digitalWrite(motor1pin2, LOW);
+
+   digitalWrite(motor2pin1, HIGH);
+   digitalWrite(motor2pin2, LOW);
+}
+
+void forward() {
+   analogWrite(EnableA, motorspeed);
+   analogWrite(EnableB, motorspeed);
+   digitalWrite(motor1pin1, LOW);
+   digitalWrite(motor1pin2, HIGH);
+
+   digitalWrite(motor2pin1, LOW);
+   digitalWrite(motor2pin2, HIGH);
+}
+
+void halt() {       // stops all motors
+   digitalWrite(motor1pin1, LOW);
+   digitalWrite(motor1pin2, LOW);
+
+   digitalWrite(motor2pin1, LOW);
+   digitalWrite(motor2pin2, LOW);
+}
+
+void turnleft() {
+   digitalWrite(EnableA, HIGH);
+   digitalWrite(EnableB, HIGH);
+   digitalWrite(motor1pin1, HIGH);
+   digitalWrite(motor1pin2, LOW);
+
+   digitalWrite(motor2pin1, LOW);
+   digitalWrite(motor2pin2, HIGH);
+}
+
+void turnright() {
+   digitalWrite(EnableA, HIGH);
+   digitalWrite(EnableB, HIGH);
+   digitalWrite(motor1pin1, LOW);
+   digitalWrite(motor1pin2, HIGH);
+
+   digitalWrite(motor2pin1, HIGH);
+   digitalWrite(motor2pin2, LOW);
+}                                              
+
+
---
+++ Joystick_TX.ino
@@ -0,0 +1,58 @@
+/* Example: nRF24L01 
+ 
+   1 - GND 
+   2 - VCC 3.3V  
+   3 - CE - Arduino pin 9
+   4 - CSN - Arduino pin 10
+   5 - SCK - Arduino pin 13
+   6 - MOSI - Arduino pin 11
+   7 - MISO - Arduino pin 12
+   8 - UNUSED 
+ 
+  You can used an Analog Joystick or 2 10K potentiometers 
+ 
+   GND - Arduino GND 
+   VCC - Arduino +5V
+   X Pot - Arduino A0
+   Y Pot - Arduino A1
+ 
+/*----- Import all required Libraries -----*/
+ 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+/*----- Declare all constant pin to be used ----*/
+ 
+#define CE_PIN   7  // Uno= 9     // Mega = 48
+#define CSN_PIN  8  // Uno= 10    // Mega = 53
+
+#define JOYSTICK_X A0
+#define JOYSTICK_Y A1
+// define your additional joystick variable constants here
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; // This is the transmit pipe to communicate the two module 
+/*-----Object Declaration ----*/ 
+RF24 radio(CE_PIN, CSN_PIN); // Activate  the Radio
+ 
+/*-----Declaration of Variables -----*/
+int joystick[3];  // Six element array holding the Joystick readings
+ 
+void setup()   
+{
+  Serial.begin(57600); /* Opening the Serial Communication */
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openWritingPipe(pipe);
+}//--(end setup )---
+ 
+void loop()   /* Runs Continuously */
+{
+  joystick[0] = map(analogRead(JOYSTICK_Y), 0, 1023, 0, 179); // Reading Analog Y
+  joystick[1] = map(analogRead(JOYSTICK_X), 0, 1023, 0, 179); // Reading Analog X
+
+  // this is where you place your additional joystick array
+
+  radio.write( joystick, sizeof(joystick) );
+
+}//--(end main loop )---
